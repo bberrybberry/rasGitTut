890fc747005804080c6721dc2522c976419104dc
Breanna Higgins <bbhiggins520@outlook.com>
2018-01-28 16:32:52 -0500

    Added a simple hello world program

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+ * This is a simple main program written to demostrate the usage of git
+ *
+ *	Author:	Breanna Higgins
+ *	Date:	01-28-2018 
+ */
+#include <stdio.h>
+
+int main(){
+	printf("Hello world!");
+	
+	return 1;
+}
